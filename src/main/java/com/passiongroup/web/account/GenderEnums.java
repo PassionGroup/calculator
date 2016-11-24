7c82c0d08e6ae0c7c784491ae327dde333f96e02
shifenghua <332560547@qq.com>
2016-11-24 12:34:39 +0800

    GenderEnums,UserInfo have changed

--- src/main/java/com/passiongroup/web/account/GenderEnums.java
+++ src/main/java/com/passiongroup/web/account/GenderEnums.java
@@ -10,5 +10,41 @@ package com.passiongroup.web.account;
  * @version $Id: GenderEnums.java, V 0.1 2016-10-31 19:02 Finderhu Exp $
  */
 public enum GenderEnums {
-    // TODO: 2016/10/31 @石丰华 
+// TODO: 2016/10/31 @石丰华
+private Integer code=new Integer();
+public enum Gender {男,女,String value=null}
+
+public void GetGender(){
+        return gender;
+        }
+public void SetGender(Sring gender){
+        this.gender=gender;
+        }
+
+public String Value (String v){
+        value=v;
+        }
+public class Description{
+    public static void main(String args[]){
+
+        Gender gender=Gender.男
+        public String Description(){
+            switch(gender){
+                case 男:
+                    System.out.println("性别："gender) ;
+                    Gender gender=Gender.女;
+                    break;
+                case 女:
+                    System.out.println("性别："gender) ;
+                    Gender gender=value;
+                    break;
+                case value:
+                    System.out.println("性别："gender) ;
+                    break;
+            }
+        }
+
+    }
+}
+
 }
--- src/main/java/com/passiongroup/web/account/UserInfo.java
+++ src/main/java/com/passiongroup/web/account/UserInfo.java
@@ -10,5 +10,35 @@ package com.passiongroup.web.account;
  * @version $Id: UserInfo.java, V 0.1 2016-10-31 19:01 Finderhu Exp $
  */
 public class UserInfo {
-    // TODO: 2016/10/31 @石丰华 
-}
+    // TODO: 2016/10/31 @石丰华
+    private String name;
+    private Integer age=new Integer();
+    private GenderEnum gender;
+    private String email;
+    private String password;
+
+    public UserInfo(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }

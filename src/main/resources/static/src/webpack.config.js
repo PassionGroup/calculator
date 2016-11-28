28612a5c0752b390eb2cf4279fe413f8b7ab6ffe
longhuai.hlh <longhuai.hlh@alipay.com>
2016-11-28 13:57:26 +0800

    配置资源生成路径

--- src/main/resources/static/src/webpack.config.js
+++ src/main/resources/static/src/webpack.config.js
@@ -1,10 +1,10 @@
-
+var path = require("path");
 module.exports = function(webpackConfig) {
   webpackConfig.babel.plugins.push('transform-runtime');
   webpackConfig.babel.plugins.push(['import', {
     libraryName: 'antd',
     style: 'css',
   }]);
-  webpackConfig.output.path = '../';
+    webpackConfig.output.path = path.join(__dirname,'..');
   return webpackConfig;
 };

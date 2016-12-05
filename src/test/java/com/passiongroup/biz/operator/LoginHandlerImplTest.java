7f62b60aba584a5fb3a6b111f997b0c39d2b35f9
finderhu <1397812937@qq.com>
2016-12-05 19:38:15 +0800

    添加登录空测试

---
+++ src/test/java/com/passiongroup/biz/operator/LoginHandlerImplTest.java
@@ -0,0 +1,21 @@
+package com.passiongroup.biz.operator;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by longhuai.hlh on 2016/12/5.
+ */
+public class LoginHandlerImplTest {
+    @Test
+    public void testLogin() throws Exception {
+
+    }
+
+    @Test
+    public void testCheckParam() throws Exception {
+
+    }
+
+}
